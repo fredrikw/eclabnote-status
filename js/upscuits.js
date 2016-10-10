f29a2ddfa8340c7b4fcc5913dd31fa3c44b321d9
Fredrik Wallner <fredrik@eclabnote.com>
2016-10-10 15:51:41 +0200

    Updates

--- js/upscuits.js
+++ js/upscuits.js
@@ -16,7 +16,7 @@
 	--
 
 	@file		upsuits.js
-	@date		Mon Oct 10 2016 15:31:41
+	@date		Mon Oct 10 2016 15:51:35
 	@author		Pixel Bakkerij
 
 	Copyright (c) 2013 Pixel Bakkerij <http://pixelbakkerij.nl>
